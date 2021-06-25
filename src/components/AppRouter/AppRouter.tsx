632be552f76e698b0494e654660a32967798c7bc
Nicolas <nicolasdirago@gmail.com>
2021-06-25 12:10:56 +0200

    implemented keep score on reload functionality, implented start new match functionality, improved styles, delayed show housePick, result and score

--- src/components/AppRouter/AppRouter.tsx
+++ src/components/AppRouter/AppRouter.tsx
@@ -1,13 +1,13 @@
-import { DataProvider } from '../../context/DataContext';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Box } from '@material-ui/core';
+import { DataProvider } from "../../context/DataContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Box } from "@material-ui/core";
 
-import { Header } from '../Header/Header';
-import { Board } from '../Board/Board';
-import { Game } from '../Game/Game';
-import { RulesModal } from '../RulesModal/RulesModal';
+import { Header } from "../Header/Header";
+import { Board } from "../Board/Board";
+import { Game } from "../Game/Game";
+import { RulesModal } from "../RulesModal/RulesModal";
 
-import { useStyles } from './styles';
+import { useStyles } from "./styles";
 
 export const AppRouter = () => {
   const classes = useStyles();
@@ -18,8 +18,8 @@ export const AppRouter = () => {
         <Header />
         <Router>
           <Switch>
-            <Route exact path='/' component={Board} />
-            <Route exact path='/game' component={Game} />
+            <Route exact path="/" component={Board} />
+            <Route exact path="/game" component={Game} />
           </Switch>
         </Router>
         <RulesModal />
